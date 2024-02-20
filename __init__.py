ccf250633135e6a127445c6699bb6919062d1476
Lucas-FB <40270359+Lucas-FB@users.noreply.github.com>
2024-02-20 14:04:26 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@ def contact():
 def ReadBDD():
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients;')
+    cursor.execute('SELECT * FROM livres;')
     data = cursor.fetchall()
     conn.close()
     
